1fb23471d5912b3cee8d6e383f7d34d3e1d2fd79
Tyler-Stocks <tylerjstocks@gmail.com>
2025-09-08 02:17:00 -0700

    Added opmodes package with skeleton TeleOp.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.kt
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.opmodes
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.frozenmilk.mercurial.Mercurial
+
+@TeleOp(group = "$")
+@Mercurial.Attach
+class TeleOp: OpMode() {
+
+    override fun init() {}
+
+    override fun loop() {}
+}
