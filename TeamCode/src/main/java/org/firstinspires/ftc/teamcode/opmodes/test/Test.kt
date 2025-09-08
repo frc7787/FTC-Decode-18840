e2ff10010d0e39de6cb002d0e2635d3807b2f926
Tyler-Stocks <tylerjstocks@gmail.com>
2025-09-08 02:19:51 -0700

    Added auto and test packages each with skeleton opmodes inside of them.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/Test.kt
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.opmodes.test
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.frozenmilk.mercurial.Mercurial
+
+@TeleOp(group = "Test")
+@Mercurial.Attach
+class Test: OpMode() {
+
+    override fun init() {}
+
+    override fun loop() {}
+}
