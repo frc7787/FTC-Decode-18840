2fbcf1a3a255668a53a533b8b879275ac999d201
Tyler-Stocks <tylerjstocks@gmail.com>
2025-09-08 22:37:36 -0700

    Added skeleton Intake Subsystem.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/Intake.kt
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.subsytems
+
+import dev.frozenmilk.dairy.core.dependency.Dependency
+import dev.frozenmilk.dairy.core.dependency.annotation.SingleAnnotation
+import dev.frozenmilk.dairy.core.wrapper.Wrapper
+import dev.frozenmilk.mercurial.subsystems.Subsystem
+
+object Intake: Subsystem {
+    // ---------------------------------------------------------------------------------------------
+    // Hardware
+
+    // ---------------------------------------------------------------------------------------------
+    // Config
+
+    var configuration = Configuration()
+
+    data class Configuration(val debug: Boolean = true)
+
+    // ---------------------------------------------------------------------------------------------
+    // State
+
+    // ---------------------------------------------------------------------------------------------
+    // Hooks
+
+    override fun postUserInitHook(opMode: Wrapper) {}
+
+    override fun preUserLoopHook(opMode: Wrapper) {}
+
+    // ---------------------------------------------------------------------------------------------
+    // Commands
+
+    // ---------------------------------------------------------------------------------------------
+    // Subsystem Boilerplate
+
+    override var dependency: Dependency<*>
+            = Subsystem.DEFAULT_DEPENDENCY and SingleAnnotation(Attach::class.java)
+
+    @Retention(AnnotationRetention.RUNTIME)
+    @Target(AnnotationTarget.CLASS)
+    @MustBeDocumented
+    annotation class Attach
+
+    // ---------------------------------------------------------------------------------------------
+}
