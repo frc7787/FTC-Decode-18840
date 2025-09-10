b5a55e1e41e86ee76df06a3312dc11c0182fbbae
Tyler-Stocks <tylerjstocks@gmail.com>
2025-09-09 22:23:26 -0700

    Attached OctoQuad feature to main opmode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.kt
@@ -3,11 +3,13 @@ package org.firstinspires.ftc.teamcode.opmodes
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import dev.frozenmilk.mercurial.Mercurial
+import org.firstinspires.ftc.teamcode.features.OctoQuad
 import org.firstinspires.ftc.teamcode.subsytems.DriveBase
 import org.firstinspires.ftc.teamcode.subsytems.Intake
 import org.firstinspires.ftc.teamcode.subsytems.TurnTable
 
 @TeleOp(group = "$")
+@OctoQuad.Attach
 @DriveBase.Attach
 @Intake.Attach
 @TurnTable.Attach
