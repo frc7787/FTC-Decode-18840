6d9030ca76a85f2178a4b0f255b8a939f41eb77f
Tyler-Stocks <tylerjstocks@gmail.com>
2025-09-10 13:13:35 -0700

    Added turret homing command to main TeleOp.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.kt
@@ -4,24 +4,22 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import dev.frozenmilk.mercurial.Mercurial
 import org.firstinspires.ftc.teamcode.features.OctoQuad
-import org.firstinspires.ftc.teamcode.subsytems.DriveBase
 import org.firstinspires.ftc.teamcode.subsytems.Intake
 import org.firstinspires.ftc.teamcode.subsytems.TurnTable
+import org.firstinspires.ftc.teamcode.subsytems.Turret
 
 @TeleOp(group = "$")
 @OctoQuad.Attach
-@DriveBase.Attach
 @Intake.Attach
 @TurnTable.Attach
+@Turret.Attach
 @Mercurial.Attach
 class TeleOp: OpMode() {
 
-    override fun init() {
-        DriveBase.defaultCommand = DriveBase.drive(
-            Mercurial.gamepad1.leftStickY,
-            Mercurial.gamepad1.leftStickX,
-            Mercurial.gamepad1.rightStickX
-        )
+    override fun init() {}
+
+    override fun start() {
+        Turret.home().schedule()
     }
 
     override fun loop() {}
