f121c74858079d69bf7377ea064e6c89b304f550
Tyler-Stocks <tylerjstocks@gmail.com>
2026-01-23 14:08:11 -0800

    Intake Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/BlueAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/BlueAuto.kt
@@ -1,30 +1,20 @@
 package org.firstinspires.ftc.teamcode.opmodes.auto
 
 import com.bylazar.configurables.annotations.Configurable
-import com.pedropathing.follower.Follower
 import com.pedropathing.geometry.BezierLine
 import com.pedropathing.geometry.Pose
-import com.pedropathing.paths.Path
 import com.pedropathing.paths.PathChain
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODER
 import com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER
 import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.util.ElapsedTime
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.WhiteBalanceControl
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants
-import org.firstinspires.ftc.teamcode.subsystems.AprilTagSubsystem
 import org.firstinspires.ftc.teamcode.subsystems.Flywheel
+import org.firstinspires.ftc.teamcode.subsystems.Intake
 import org.firstinspires.ftc.teamcode.subsystems.Spindexer
 import org.firstinspires.ftc.teamcode.subsystems.Transfer
-import org.firstinspires.ftc.teamcode.util.Motif
-import org.firstinspires.ftc.vision.VisionPortal
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor
 
 @Autonomous(group = "Blue")
 @Configurable
@@ -33,10 +23,16 @@ class BlueAuto: LinearOpMode() {
     companion object {
 
         @JvmField
-        var START = Pose(23.0, 124.0, Math.toRadians(143.0))
+        var START = Pose(21.0, 123.5, Math.toRadians(143.0))
 
         @JvmField
-        var SHOOT = Pose(37.0, 116.0, Math.toRadians(143.0))
+        var SHOOT = Pose(36.0, 113.0, Math.toRadians(143.0))
+
+        @JvmField
+        var INTAKE_START = Pose(44.0, 93.0, Math.toRadians(180.0))
+
+        @JvmField
+        var INTAKE_END = Pose(23.0, 93.0, Math.toRadians(180.0))
 
         @JvmField
         var END = Pose(53.0, 125.0, Math.toRadians(143.0))
@@ -78,6 +74,10 @@ class BlueAuto: LinearOpMode() {
         }
     }
 
+    private val intake by lazy {
+        Intake(hardwareMap)
+    }
+
     private val transfer by lazy {
         Transfer(hardwareMap)
     }
@@ -90,6 +90,22 @@ class BlueAuto: LinearOpMode() {
             .setLinearHeadingInterpolation(START.heading, SHOOT.heading)
             .build()
 
+        val shootToIntakeStart = follower.pathBuilder()
+            .addPath(BezierLine(SHOOT, INTAKE_START))
+            .setLinearHeadingInterpolation(SHOOT.heading, INTAKE_START.heading)
+            .build()
+
+        val intakeStartToEnd = follower.pathBuilder()
+            .addPath(BezierLine(INTAKE_START, INTAKE_END))
+            .setLinearHeadingInterpolation(INTAKE_START.heading, INTAKE_END.heading)
+            .setVelocityConstraint(0.3)
+            .build()
+
+        val intakeEndToShoot = follower.pathBuilder()
+            .addPath(BezierLine(INTAKE_END, SHOOT))
+            .setLinearHeadingInterpolation(INTAKE_END.heading, SHOOT.heading)
+            .build()
+
         val shootToEnd = follower.pathBuilder()
             .addPath(BezierLine(SHOOT, END))
             .setLinearHeadingInterpolation(SHOOT.heading, END.heading)
@@ -99,9 +115,9 @@ class BlueAuto: LinearOpMode() {
 
         flywheel.targetRPM = 4000.0
 
-        followPath(startToShoot)
+        followPath(startToShoot, intaking = false)
 
-        spindexerToSlot(index = 0)
+        spindexerToSlot(index = 2)
         transfer(time = 50)
 
         wait(800.0)
@@ -111,10 +127,22 @@ class BlueAuto: LinearOpMode() {
 
         wait(800.0)
 
-        spindexerToSlot(index = 2)
+        spindexerToSlot(index = 0)
         transfer(time = 50)
 
-        followPath(shootToEnd)
+        followPath(shootToIntakeStart, intaking = false)
+
+        wait(800.0)
+
+        followPath(intakeStartToEnd, intaking = true)
+
+        wait(800.0)
+
+        followPath(intakeEndToShoot, intaking = false)
+
+        wait(800.0)
+
+        followPath(shootToEnd, intaking = false)
     }
 
     private fun wait(time: Double) {
@@ -126,11 +154,19 @@ class BlueAuto: LinearOpMode() {
         }
     }
 
-    private fun followPath(path: PathChain) {
+    private fun followPath(path: PathChain, intaking: Boolean) {
         follower.followPath(path)
         follower.update()
 
-        while (follower.isBusy && !isStopRequested) {
+        while (follower.isBusy && !isStopRequested && opModeIsActive()) {
+            if (intaking) {
+                intake.power = 1.0
+                spindexer.targetPower = 0.4
+            } else {
+                intake.power = 0.0
+            }
+
+            intake.update()
             follower.update()
             flywheel.update()
             spindexer.update()
@@ -143,7 +179,9 @@ class BlueAuto: LinearOpMode() {
         spindexer.toSlot(index, false)
         spindexer.update()
 
-        while (!spindexer.atPosition && !isStopRequested && opModeIsActive()) {
+        val timer = ElapsedTime()
+
+        while (!spindexer.atPosition && !isStopRequested && opModeIsActive() && timer.seconds() < 1.0) {
             telemetry.addLine("To Position")
             telemetry.update()
             flywheel.update()
