64daf55ee519f31ec3f92eb419e7cc7093ceba50
Tyler Stocks <tylerjstocks@gmail.com>
2026-06-24 17:45:46 -0700

    Added decode specific data structures

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/Artifact.kt
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.decode
+
+data class Artifact(val color: ArtifactColor)
+
+enum class ArtifactColor {
+    PURPLE,
+    GREEN
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/decode/Motif.kt
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.decode
+
+enum class Motif {
+    PPG,
+    PGP,
+    GPP,
+}
