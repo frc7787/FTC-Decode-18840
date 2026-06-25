0423c93177eb56cf55132fd862d3e6915f6341ea
Tyler Stocks <tylerjstocks@gmail.com>
2026-06-24 17:05:24 -0700

    Added top level 'Robot' class to simplify TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.kt
@@ -13,36 +13,16 @@ import kotlin.math.abs
 @TeleOp(group = "$")
 class TeleOp: OpMode() {
 
-    private val intake by lazy {
-        Intake(hardwareMap)
-    }
-
-    private val flywheel by lazy {
-        Flywheel(hardwareMap)
-    }
-
-    private val spindexer by lazy {
-        Spindexer(hardwareMap)
-    }
-
-    private val transfer by lazy {
-        Transfer(hardwareMap)
-    }
-
-    private val camera by lazy {
-        Camera(hardwareMap)
-    }
-
-    private val follower by lazy {
-        Constants.createFollower(hardwareMap)
+    private val robot by lazy {
+        Robot(hardwareMap)
     }
 
     override fun init() {
         schedule(
             conditional(
                 { flywheelToggle },
-                flywheel.spinAt(3000.0),
-                flywheel.spinAt(0.0)
+                robot.flywheel.spinAt(3000.0),
+                robot.flywheel.spinAt(0.0)
             )
         )
     }
@@ -50,54 +30,56 @@ class TeleOp: OpMode() {
     private var flywheelToggle = false
 
     override fun loop() {
-        camera.detectedTags().forEach { detection ->
-            telemetry.addLine("Tag Detected With ID: ${detection.id}")
-        }
-
-        if (gamepad1.circleWasReleased()) {
-            schedule(
-                parallel(
-                    transfer.up(),
-                    spindexer.power(0.5)
-                )
-            )
-        }
-
-        if (gamepad1.triangleWasReleased()) {
-            schedule(
-                deadline(
-                    spindexer.toNextIntakingPosition(),
-                    transfer.down()
-                )
-            )
-        }
-
-        if (gamepad1.leftBumperWasReleased()) {
-            schedule(intake.intake())
-        }
-
-        if (gamepad1.rightBumperWasReleased()) {
-            schedule(intake.intake())
-        }
-
-        if (gamepad1.squareWasReleased()) {
-            flywheelToggle = !flywheelToggle
-        }
-
-        follower.setTeleOpDrive(
-            run {
-                val raw = -gamepad1.left_stick_y.toDouble()
-                raw * abs(raw)
-            },
-            run {
-                val raw = gamepad1.left_stick_x.toDouble()
-                raw * abs(raw)
-            },
-            run {
-                val raw = gamepad1.right_stick_x.toDouble()
-                raw * abs(raw)
+        with (robot) {
+            camera.detectedTags().forEach { detection ->
+                telemetry.addLine("Tag Detected With ID: ${detection.id}")
             }
-        )
+
+            if (gamepad1.circleWasReleased()) {
+                schedule(
+                    parallel(
+                        transfer.up(),
+                        spindexer.power(0.5)
+                    )
+                )
+            }
+
+            if (gamepad1.triangleWasReleased()) {
+                schedule(
+                    deadline(
+                        spindexer.toNextIntakingPosition(),
+                        transfer.down()
+                    )
+                )
+            }
+
+            if (gamepad1.leftBumperWasReleased()) {
+                schedule(intake.intake())
+            }
+
+            if (gamepad1.rightBumperWasReleased()) {
+                schedule(intake.intake())
+            }
+
+            if (gamepad1.squareWasReleased()) {
+                flywheelToggle = !flywheelToggle
+            }
+
+            follower.setTeleOpDrive(
+                run {
+                    val raw = -gamepad1.left_stick_y.toDouble()
+                    raw * abs(raw)
+                },
+                run {
+                    val raw = gamepad1.left_stick_x.toDouble()
+                    raw * abs(raw)
+                },
+                run {
+                    val raw = gamepad1.right_stick_x.toDouble()
+                    raw * abs(raw)
+                }
+            )
+        }
 
         Scheduler.execute()
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Robot.kt
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.subsystems
+
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.teamcode.pedropathing.Constants
+
+class Robot(hardwareMap: HardwareMap) {
+    val flywheel  = Flywheel(hardwareMap)
+    val intake    = Intake(hardwareMap)
+    val transfer  = Transfer(hardwareMap)
+    val spindexer = Spindexer(hardwareMap)
+    val camera    = Camera(hardwareMap)
+    val follower  = Constants.createFollower(hardwareMap)
+}
